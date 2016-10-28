ce9f4b7e25a94809e2231a28589246a4bcd2618c
LaurentDardenne <LaDardenne@Orange.fr>
2016-10-28 17:31:59 +0200

    Aborder les problèmes de portée entre un scriptblock et un module.

---
+++ La portée d’un scriptblock dans un module/Sources/Edit-String.ps1
@@ -0,0 +1,10 @@
+﻿Function Edit-String {
+  param(
+   [string]$pattern,
+   [string] $Text,
+   [scriptblock] $evaluator 
+  )         
+ 
+ $Regex=New-Object System.Text.RegularExpressions.RegEx $pattern
+ $Regex.Replace($Text, $evaluator)
+}
